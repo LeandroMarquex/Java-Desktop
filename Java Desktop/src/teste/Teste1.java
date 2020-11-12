41b870ad6c2d4efeeb58d67055674438968bf573
Leandro Marques <Leandro Marques@SERVER>
2020-11-12 19:17:04 -0300

    Teste de atualização.

--- Java Desktop/src/teste/Teste1.java
+++ Java Desktop/src/teste/Teste1.java
@@ -15,6 +15,10 @@ public class Teste1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("testes");
+		System.out.println("testes");
+		System.out.println("testes");
+		System.out.println("testes");
+		System.out.println("testes");
 	}
 
 }
