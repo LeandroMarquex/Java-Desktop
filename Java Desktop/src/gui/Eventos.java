1840238ffa56bb09fe1b7047863b081dc098dfc4
Leandro Marques <Leandro Marques@SERVER>
2020-11-15 13:57:52 -0300

    Aula 39 - Label Propriedades

--- Java Desktop/src/gui/Eventos.java
+++ Java Desktop/src/gui/Eventos.java
@@ -3,11 +3,14 @@
  */
 package gui;
 
+import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 
 /**
@@ -23,14 +26,26 @@ public class Eventos extends JFrame implements ActionListener{
 	
 		super("Eventos");
 		
+		JLabel simples = new JLabel("Label Teste");
+		simples.setToolTipText("Meu label");
 		
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		c.add(simples);
+		
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300, 300);
+		setVisible(true);
+		
+		/*
 		getContentPane().add(botao);
 		botao.addActionListener(this);
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(300, 300);
 		setVisible(true);
-		
+		*/
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
