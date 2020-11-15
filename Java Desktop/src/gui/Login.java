373a1d0d86aa3f08eb6c37c7787206d059f65ebd
Leandro Marques <Leandro Marques@SERVER>
2020-11-15 14:35:05 -0300

    Aula 41 - Criando Login

--- Java Desktop/src/gui/Login.java
+++ Java Desktop/src/gui/Login.java
@@ -6,8 +6,11 @@ package gui;
 import java.awt.Container;
 import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 /**
  * @author Leandro Marques
@@ -15,15 +18,19 @@ import javax.swing.JLabel;
  */
 public class Login extends JFrame{
 
-	/**
-	 * 
-	 */
+	JButton logar, cancelar;
+	
 	public Login() {
 		super("Login");
 		
 		Container c = getContentPane();
 		c.setLayout(new GridLayout(3, 2));
 		c.add(new JLabel("Login:"));
+		c.add(new JTextField());
+		c.add(new JLabel("Senha:"));
+		c.add(new JPasswordField());
+		c.add(new JButton("Logar:"));
+		c.add(new JButton("Cancelar:"));
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(300, 300);
